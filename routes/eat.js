19732fecb04ef7b1e7ef408d9531bfc1d8829fa5
Waiting7777 <waiting0819@msn.com>
2017-03-13 18:12:20 +0800

    change list string

--- routes/eat.js
+++ routes/eat.js
@@ -108,7 +108,7 @@ router.post('/', function(req, res, next) {
 
   if(type == 'text'){
 
-    if(text == '台北' || text == '新北' || text == '中午' || test == '#'){
+    if(text == '台北' || text == '新北' || text == '中午' || test == '列表'){
 
       switch (text) {
         case '台北':
@@ -123,7 +123,7 @@ router.post('/', function(req, res, next) {
             "text": lunch[Math.floor(Math.random()*lunch.length)]
           }
           break
-        case '#':
+        case '列表':
           replycontent = {
             "type": "text",
             "text": lunch.toString()
