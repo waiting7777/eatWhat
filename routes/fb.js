92bf9af006fe01a9fbf36956d77f4c21ae5934d8
Waiting7777 <waiting0819@msn.com>
2017-03-14 17:09:04 +0800

    fb token

--- routes/fb.js
+++ routes/fb.js
@@ -6,7 +6,7 @@ var request = require('request');
 module.exports = router;
 
 router.get('/', function(req, res, next) {
-  if (req.query['hub.verify_token'] === <'EAAXWlMg1DP0BABZB8WHaVJWFQMZBc0HTzzVinXTJSFOQsDsoCTViNL0HtrawPHluJEi9OBnOxPNpKuNHdaf7Q4qbYg5mN1vVJVui2bUBuVnnZBmOIpgDwjrVKtjZAYycS3NP3422EbgvZBQZCLkOYazcDdosMhYaSBdmCNz61iigZDZD'>) {
+  if (req.query['hub.verify_token'] === 'EAAXWlMg1DP0BABZB8WHaVJWFQMZBc0HTzzVinXTJSFOQsDsoCTViNL0HtrawPHluJEi9OBnOxPNpKuNHdaf7Q4qbYg5mN1vVJVui2bUBuVnnZBmOIpgDwjrVKtjZAYycS3NP3422EbgvZBQZCLkOYazcDdosMhYaSBdmCNz61iigZDZD') {
     res.send(req.query['hub.challenge']);
   } else {
     res.send('Error, wrong validation token');
